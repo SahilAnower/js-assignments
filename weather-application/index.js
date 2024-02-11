0a93a60fafbad48baeed5578907fd209d99aedea
SahilAnower <sahilanower2@gmail.com>
2024-02-12 02:43:45 +0530

    Added the functionality for forecast

--- weather-application/index.js
+++ weather-application/index.js
@@ -2,8 +2,8 @@
 
 // todo: display a forecast for upcoming days
 
-const weatherApiKey = "800d9da25amshc3eb8eb4088b19fp102153jsn5a6f03227cc5";
-const weatherApiUrl = "https://open-weather13.p.rapidapi.com";
+const weatherApiKey = "dff95c718e47360648a8d56ec975d0ee";
+const weatherApiUrl = "https://api.openweathermap.org/data/2.5";
 
 const unsplashWeatherImageUrl = "https://source.unsplash.com/random/?";
 
@@ -17,11 +17,13 @@ const minTemperatureElement = document.getElementById("min-temperature");
 const maxTemperatureElement = document.getElementById("max-temperature");
 const pressureElement = document.getElementById("pressure");
 const visibilityElement = document.getElementById("visibility");
+const forecastContainer = document.getElementById("forecast-append-container");
 
 searchButton.addEventListener("click", () => {
   const location = locationInput.value.trim();
   if (location) {
     fetchWeather(location);
+    getFiveDaysWeatherForecast(location);
   }
 });
 
@@ -30,26 +32,20 @@ locationInput.addEventListener("keydown", (event) => {
     const location = locationInput.value.trim();
     if (location) {
       fetchWeather(location);
+      getFiveDaysWeatherForecast(location);
     }
   }
 });
 
-const fahrenheitToCelsius = (temp) => {
-  return Math.round(((temp - 32) * 5) / 9);
+const kelvinToCelsius = (temp) => {
+  return Math.round(temp - 273.15);
 };
 
 async function fetchWeather(location) {
-  const url = `${weatherApiUrl}/city/${location}`;
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": weatherApiKey,
-      "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
-    },
-  };
+  const url = `${weatherApiUrl}/weather?q=${location}&appid=${weatherApiKey}`;
 
   try {
-    const response = await fetch(url, options);
+    const response = await fetch(url);
     const data = await response.json();
     if (data?.cod != 200) {
       alert(data?.message);
@@ -57,22 +53,25 @@ async function fetchWeather(location) {
     }
     const locationName = data?.name;
     const currentTemperatureInCelsius = Math.round(
-      fahrenheitToCelsius(data?.main?.temp)
+      kelvinToCelsius(data?.main?.temp)
     );
     const weatherDescription = data?.weather[0]?.main;
     const feelsLikeTemperatureInCelsius = Math.round(
-      fahrenheitToCelsius(data?.main?.feels_like)
+      kelvinToCelsius(data?.main?.feels_like)
     );
     const minTemperatureInCelsius = Math.round(
-      fahrenheitToCelsius(data?.main?.temp_min)
+      kelvinToCelsius(data?.main?.temp_min)
     );
     const maxTemperatureInCelsius = Math.round(
-      fahrenheitToCelsius(data?.main?.temp_max)
+      kelvinToCelsius(data?.main?.temp_max)
     );
     const pressure = data?.main?.pressure; // in mb
     const visibility = Number(data?.visibility) / 1000.0;
-    const longitude = data?.coord?.lon; // use for forecast
-    const latitude = data?.coord?.lat; // use for forecast
+    // const longitude = data?.coord?.lon; // use for forecast
+    // const latitude = data?.coord?.lat; // use for forecast
+    document.querySelectorAll(".extra-property").forEach((div) => {
+      div.classList.remove("hidden");
+    });
     locationElement.textContent = locationName;
     temperatureElement.textContent = `${currentTemperatureInCelsius}°C`;
     descriptionElement.textContent = weatherDescription;
@@ -93,4 +92,41 @@ async function updateWeatherImage(weather) {
   document.body.style.backgroundSize = "cover";
   document.body.style.backgroundPosition = "center";
   document.body.style.backgroundRepeat = "no-repeat";
+}
+
+async function getFiveDaysWeatherForecast(location) {
+  const url = `${weatherApiUrl}/forecast?q=${location}&appid=${weatherApiKey}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data?.cod != 200) {
+      alert(data?.message);
+      return;
+    }
+    // console.log(data);
+    document
+      .querySelectorAll(".forecast-header")
+      .forEach((h) => h.classList.remove("hidden"));
+    forecastContainer.innerHTML = "";
+    const total = data?.cnt;
+    const eachDaySize = total / 5;
+    for (let i = 0; i < total; i += eachDaySize) {
+      let sumTemp = 0;
+      let avgTemp = 0;
+      let date = data?.list[i]?.dt_txt.split(" ")[0];
+      for (let j = i; j < i + eachDaySize; j++) {
+        sumTemp += Number(data?.list[i]?.main?.temp);
+      }
+      avgTemp = kelvinToCelsius(sumTemp / eachDaySize);
+      const newDiv = document.createElement("div");
+      newDiv.innerHTML = `
+        <p>${avgTemp} °C</p>
+        <p>${date}</p>
+      `;
+      forecastContainer.appendChild(newDiv);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
